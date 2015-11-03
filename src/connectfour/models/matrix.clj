07c76d4e2ec6ea8cd17b412451892d791c226c0a
Joshua Cano <jcano@monsooncommerce.com>
2015-11-02 23:47:33 -0800

    Added comments

--- src/connectfour/models/matrix.clj
+++ src/connectfour/models/matrix.clj
@@ -4,26 +4,40 @@
 
 (def playmatrix (atom [{:spot1 "x" :spot2 0 :spot3 0} {:spot1 0 :spot2 0 :spot3 0} {:spot1 0 :spot2 0 :spot3 0}]))
 
-(defn keyify [x]
+(defn keyify 
+  "Keyify the column names for keys of matri"
+  [x]
   (keyword (str "spot" (+ x 1))))
 
-(defn get-keys [size]
+(defn get-keys 
+  "Get Keys of each spot on a board"
+  [size]
   (vec (map #(keyify %) (range size))))
 
-(defn return-row [size]
+(defn return-row 
+  "Return empty row"
+  [size]
   (zipmap (get-keys size) (repeat size 0)))
 
-(defn get-matrix [size]
+(defn get-matrix 
+  "Return square board based on size passed in"
+  [size]
   (vec (repeat size (return-row size))))
 
-(defn init-matrix [size]
+(defn init-matrix 
+  "Initialize matrix"
+  [size]
   (if (> size 2)
     (get-matrix size)))
 
-(defn get-val [matrix [x y]]
+(defn get-val 
+  "Get value at particular index on board"
+  [matrix [x y]]
   ((get matrix y) (keyify x)))
 
-(defn taken [matrix [x y]]
+(defn taken? 
+  "Is space taken?"
+  [matrix [x y]]
   (not (= (get-val matrix [x y]) 0)))
 
 (defn set-val 
@@ -31,31 +45,43 @@
   [matrix [x y] xo]
   (assoc-in matrix [y (keyify x)] xo))
 
-(defn set-permanent-val [[x y] xo]
-  (if (not (taken @playmatrix [x y])) 
+(defn set-permanent-val 
+  "Set Permanent value in Matrix atom for system"
+  [[x y] xo]
+  (if-not (taken? @playmatrix [x y])
     (do 
       (swap! playmatrix assoc-in [y (keyify x)] xo)
       true)
     false))
 
-(defn init-permanent-matrix [size]
-  (if (> size 2)
+(defn init-permanent-matrix 
+  "Initialize permanent gameboard"
+  [size]
+  (when (> size 2)
     (reset! playmatrix (get-matrix size))))
 
-(defn find-all-available [matrix]
+(defn find-all-available 
+  "Return all available spaces on board"
+  [matrix]
   (let [size (count matrix)]
     (for [x (range size) 
           y (range size)
           :when (= 0 (get-val matrix [x y]))]
       [x y])))
 
-(defn count-available [matrix]
+(defn count-available 
+  "Count available spaces on board"
+  [matrix]
    (count (find-all-available matrix)))
 
-(defn get-game-board []
+(defn get-game-board 
+  "Return board"`
+  []
   @playmatrix)
 
-(defn count-val [matrix mark]
+(defn count-val 
+  "Count instances of a particular mark on board"
+  [matrix mark]
   (filter #(= mark %) (flatten (map vals matrix))))
 
   (defn your-turn? 
