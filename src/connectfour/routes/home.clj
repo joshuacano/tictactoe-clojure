7e6e4618d24966216de4685a30356b7c204e10de
Ubuntu <ubuntu@ip-172-31-28-216.us-west-2.compute.internal>
2015-11-03 07:43:12 +0000

    Minor edits

--- src/connectfour/routes/home.clj
+++ src/connectfour/routes/home.clj
@@ -1,57 +1,65 @@
 (ns connectfour.routes.home
-  (:require [connectfour.layout :as layout]
-            [connectfour.models.playbrain :refer :all]
-            [connectfour.models.minmax :as mm]
-            [connectfour.models.alphabeta :as ab]
-            [connectfour.models.matrix :as mat]
-            [connectfour.models.validate :refer :all]
-            [compojure.core :refer [defroutes GET POST]]
-            [ring.util.http-response :refer [ok]]
-	    [taoensso.timbre :refer [trace debug info warn error fatal]]
-            [clojure.java.io :as io]))
+ (:require [connectfour.layout :as layout]
+  [connectfour.models.playbrain :refer :all]
+  [connectfour.models.minmax :as mm]
+  [connectfour.models.alphabeta :as ab]
+  [connectfour.models.matrix :as mat]
+  [connectfour.models.validate :refer :all]
+  [compojure.core :refer [defroutes GET POST]]
+  [ring.util.http-response :refer [ok]]
+  [taoensso.timbre :refer [trace debug info warn error fatal]]
+  [clojure.java.io :as io]))
 
 (defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+ (layout/render
+  "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
 (defn about-page []
-  (layout/render "about.html"))
+ (layout/render "about.html"))
 
 (defn return-json [returnVal]
-{:status 200 
+"Helper function to return json"
+ {:status 200 
  :headers {"Content-Type" "application/json"} 
  :body returnVal})
 
 (defn init-page []
-  (let [rn (mat/init-permanent-matrix 3)]
+"Initialize game board"
+ (info "Initialize Game Board")
+ (let [rn (mat/init-permanent-matrix 3)]
   (return-json (mat/get-game-board))))
 
-(defn matrix-page []
+(defn matrix-page 
+"Debugging page to display matrix"
+[]
  (return-json (mat/get-game-board)))
 
-(defn set-and-return-computer-move [xindex yindex]
+(defn set-and-return-computer-move 
+ "Set user move and return computer move"
+ [xindex yindex]
  (let [result {:winner nil :x nil :y nil}]
- (do
-  (mat/set-permanent-val [xindex yindex] "x")
-  (if (did-somebody-win? (mat/get-game-board)) (assoc result :winner (who-won? (mat/get-game-board)))
-   (do 
-    (let [best-move (ab/get-best-move (mat/get-game-board))
-          saved (mat/set-permanent-val best-move "y")
-          won (if (did-somebody-win? (mat/get-game-board)) (who-won? (mat/get-game-board)) nil)]
-      (assoc result :winner won :x (first best-move) :y (second best-move))))))))
+  (do
+   (mat/set-permanent-val [xindex yindex] "x")
+   (if (did-somebody-win? (mat/get-game-board)) (assoc result :winner (who-won? (mat/get-game-board)))
+     (let [best-move (ab/get-best-move (mat/get-game-board))
+      saved (mat/set-permanent-val best-move "y")
+      won (if (did-somebody-win? (mat/get-game-board)) (who-won? (mat/get-game-board)) nil)]
+      (assoc result :winner won :x (first best-move) :y (second best-move)))))))
 
-(defn handle-move [xindex yindex]
-   (let [response (set-and-return-computer-move xindex yindex)
- 	 winner (if (string? response) response nil)
-         x (when (nil? winner) (first response))
-         y (when (nil? winner) (second response))]
-	{:winner winner :x x :y y}))
+(defn handle-move 
+ "Run user and computer move and return appropriate move to front-end"
+ [xindex yindex]
+ (let [response (set-and-return-computer-move xindex yindex)
+       winner (if (string? response) response nil)
+       x (when (nil? winner) (first response))
+       y (when (nil? winner) (second response))]
+    {:winner winner :x x :y y}))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/matrix" [] (matrix-page))
-  (GET "/init" [] (init-page))
-  (GET "/about" [] (about-page))
-  (POST "/store-move" request
-     (let [{:keys [x y]} (:params request)]
-          (return-json (set-and-return-computer-move x y)))))
+ (GET "/" [] (home-page))
+ (GET "/matrix" [] (matrix-page))
+ (GET "/init" [] (init-page))
+ (GET "/about" [] (about-page))
+ (POST "/store-move" request
+  (let [{:keys [x y]} (:params request)]
+   (return-json (set-and-return-computer-move x y)))))
